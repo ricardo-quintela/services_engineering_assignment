883b92c9d27826dd98cd09342cf1c1d22bf2d53b
ricardoquinteladev <ricardo.dev185@gmail.com>
2024-05-09 17:08:11 +0100

    fix: using only rest-framework

--- DEV/clinic/authentication/views.py
+++ DEV/clinic/authentication/views.py
@@ -1,19 +1,14 @@
-from django.shortcuts import render, redirect
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User, Group
 
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
 from .serializers import UserSerializer, GroupSerializer
-
-from .forms import AuthForm
 
 class UserViewSet(viewsets.ModelViewSet):
     """To see the created users
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = []
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
@@ -21,49 +16,4 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-def login_view(request: HttpRequest) -> HttpResponse:
-    """Logs a user in
-
-    Args:
-        request (HttpRequest): the request
-
-    Returns:
-        HttpResponse: the rendered login template
-    """
-
-    if request.method == "POST":
-        login_form = AuthForm(data=request.POST)
-
-        if login_form.is_valid():
-            username = login_form.cleaned_data["username"]
-            password = login_form.cleaned_data["password"]
-
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                login(request=request, user=user)
-
-    else:
-        login_form = AuthForm()
-
-
-    context = {
-        "login_form": login_form
-    }
-
-    return render(request, "login.html", context=context)
-
-
-def logout_view(request: HttpRequest) -> HttpResponseRedirect:
-    """Logs a user out and redirects to the home page
-
-    Args:
-        request (HttpRequest): the request
-
-    Returns:
-        HttpResponseRedirect: a redirect to the landing page
-    """
-    logout(request)
-    return redirect("/")
+    permission_classes = []
--- DEV/clinic/clinic/urls.py
+++ DEV/clinic/clinic/urls.py
@@ -2,16 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from authentication.views import UserViewSet
-from authentication.views import login_view, logout_view
-from home.views import home_page_view
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
 
 urlpatterns = [
-    path("", home_page_view),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path("login/", login_view),
-    path("logout/", logout_view)
 ]

c7dc91341b9cb75742464590c0e6d0f4510e5ab5
ricardo-quintela <ricardo.dev185@gmail.com>
2024-05-16 22:13:46 +0100

    test: fixes tests for jwt and adds register tests

--- DEV/clinic/appointments/tests.py
+++ DEV/clinic/appointments/tests.py
@@ -34,8 +34,9 @@ class TestAppointments(BaseTestCase):
 
     def test_get_appointments(self):
         """Tests if an admin can get all the appointments"""
-        self.client.cookies.load({"jwt": generate_token(self.admins[0])})
-        response = self.client.get("/appointments/")
+        response = self.client.get(
+            "/appointments/", headers={"jwt": generate_token(self.admins[0])}
+        )
 
         self.assertJSONEqual(
             response.content,
@@ -54,15 +55,19 @@ class TestAppointments(BaseTestCase):
 
     def test_get_appointments_not_admin(self):
         """Tests if a regular user is blocked from accessing the appointments data"""
-        self.client.cookies.load({"jwt": generate_token(self.users[0])})
-        response = self.client.get("/appointments/")
+        response = self.client.get(
+            "/appointments/", headers={"jwt": generate_token(self.users[0])}
+        )
 
         self.assertJSONEqual(response.content, {"error": "Forbidden."})
 
     def test_close_appointment(self):
         """Tests if an admin can alter an appointment's field"""
-        self.client.cookies.load({"jwt": generate_token(self.admins[0])})
-        response = self.client.put("/appointments/1/", data={"estado": "closed"})
+        response = self.client.put(
+            "/appointments/1/",
+            data={"estado": "closed"},
+            headers={"jwt": generate_token(self.admins[0])},
+        )
 
         self.assertJSONEqual(
             response.content,
@@ -78,18 +83,23 @@ class TestAppointments(BaseTestCase):
 
     def test_close_appointment_wrong_attribute(self):
         """Tests if the attribute's name is not changed if the name is incorrect"""
-        self.client.cookies.load({"jwt": generate_token(self.admins[0])})
-        response = self.client.put("/appointments/1/", data={"unexistent_attribute": "value"})
+        response = self.client.put(
+            "/appointments/1/",
+            data={"unexistent_attribute": "value"},
+            headers={"jwt": generate_token(self.admins[0])},
+        )
 
         self.assertJSONEqual(
-            response.content, {"error": "Appointment has no field named 'unexistent_attribute'"}
+            response.content,
+            {"error": "Appointment has no field named 'unexistent_attribute'"},
         )
 
     def test_close_appointment_not_admin(self):
         """Tests if a regular user is blocked from accessing the appointments data"""
-        self.client.cookies.load({"jwt": generate_token(self.users[0])})
-        response = self.client.put("/appointments/1/", data={"estado": "closed"})
-
-        self.assertJSONEqual(
-            response.content, {"error": "Forbidden."}
+        response = self.client.put(
+            "/appointments/1/",
+            data={"estado": "closed"},
+            headers={"jwt": generate_token(self.users[0])},
         )
+
+        self.assertJSONEqual(response.content, {"error": "Forbidden."})

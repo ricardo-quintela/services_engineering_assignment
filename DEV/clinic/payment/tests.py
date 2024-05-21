bdd2ecb51267bab8946b4f597802634d1299e467
JoaoRafaelHenriques <joao.rafael.henriques@gmail.com>
2024-05-21 16:29:43 +0100

    fix: tests for appointements

--- DEV/clinic/payment/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from clinic.tests import BaseTestCase
-
-# Create your tests here.
-class testPaymentMethods(BaseTestCase):
-    
-    def testFirstOption(self):
-        response = self.client.get("/payment/1/")
-        print(response.content)

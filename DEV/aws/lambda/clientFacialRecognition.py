579683225629b29489d47ec383614161b956fff8
ricardo-quintela <ricardo.dev185@gmail.com>
2024-05-21 16:42:57 +0100

    fix: fixes wrong state change in state machine

--- DEV/aws/lambda/clientFacialRecognition.py
+++ DEV/aws/lambda/clientFacialRecognition.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 
 clientRekognition = boto3.client("rekognition")
@@ -13,19 +12,24 @@ def lambda_handler(event, context):
     try:
         response = clientRekognition.search_faces_by_image(
             CollectionId=collection_id,
-            Image={
-                "S3Object": {
-                    "Bucket": bucket_name,
-                    "Name": image_key
-                }
-            },
-            MaxFaces=1
+            Image={"S3Object": {"Bucket": bucket_name, "Name": image_key}},
+            MaxFaces=1,
         )
     except clientRekognition.exceptions.InvalidS3ObjectException:
         return {
-                "error": f"Object with key '{image_key}' doesn't exist on bucket '{bucket_name}'."
-            }
+            "error": f"Object with key '{image_key}' doesn't exist on bucket '{bucket_name}'.",
+            "imageUUID": image_key,
+            "bucketName": bucket_name,
+        }
 
     if response["FaceMatches"]:
-        return {"faceId": response["FaceMatches"][0]["Face"]["FaceId"]}
-    return {"error": "No faces were detected."}
+        return {
+            "faceId": response["FaceMatches"][0]["Face"]["FaceId"],
+            "imageUUID": image_key,
+            "bucketName": bucket_name,
+        }
+    return {
+        "error": "No faces were detected.",
+        "imageUUID": image_key,
+        "bucketName": bucket_name,
+    }

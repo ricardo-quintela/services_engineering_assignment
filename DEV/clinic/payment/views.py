771854a3b4b7cfa1857af69cb352f5e2a6696ac0
JoaoRafaelHenriques <joao.rafael.henriques@gmail.com>
2024-05-23 14:09:33 +0100

    wip: call step function to make idempotent payment

--- DEV/clinic/payment/views.py
+++ DEV/clinic/payment/views.py
@@ -3,10 +3,24 @@ from django.http import HttpRequest, JsonResponse
 
 from rest_framework.decorators import api_view
 
+from DEV.clinic.aws_middleware.stepfunctions import execute_workflow
+from DEV.clinic.clinic.settings import STATE_MACHINE_ARN
+
 ENTIDADE = 12345
 
 @api_view(["GET"])
 def payment_option(request: HttpRequest, option: int, telemovel: int) -> JsonResponse:
+    
+    response = execute_workflow(
+        {
+            "type": "requestIdempotencyKey",
+            "appointement_id": 2
+        },
+        STATE_MACHINE_ARN
+    )
+    
+    if "error" in response.content.keys():
+        return JsonResponse({"error": "Escolha uma opção válida."})
     
     if option == 1:
         valor: float = random.randint(5, 25)
